7feca163cea417cd96208e3be6419d690e0ef852
Lars Eggert <lars@eggert.org>
2024-09-02 12:26:54 +0300

    v4

--- src/lib.rs
+++ src/lib.rs
@@ -262,8 +262,10 @@ mod test {
     fn loopback_interface_mtu_v4() {
         #[cfg(target_os = "macos")]
         check_mtu("localhost:443", true, 16384);
-        #[cfg(not(target_os = "macos"))]
-        check_mtu("localhost:443", true, 65536);
+        #[cfg(target_os = "linux")]
+        check_mtu("localhost:443", false, 65536);
+        #[cfg(target_os = "windows")]
+        check_mtu("localhost:443", false, 4294967295);
     }
 
     #[test]
