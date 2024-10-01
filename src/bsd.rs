cba785bfdb9bfa4049e44f5465efce0e53b531e2
Lars Eggert <lars@eggert.org>
2024-10-01 09:56:10 +0300

    feat: Obtain info via routing sockets
    WIP
    
    This avoids the need to mess around with sockets.

---
+++ src/bsd.rs
@@ -0,0 +1,135 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use core::str;
+use std::{ffi::c_void, io::Error, mem, net::IpAddr, ptr};
+
+use libc::{
+    close, getpid, read, rt_msghdr, sockaddr_dl, sockaddr_in, sockaddr_in6, sockaddr_storage,
+    socket, write, AF_INET, AF_INET6, PF_ROUTE, RTAX_IFP, RTAX_MAX, RTA_DST, RTA_IFP, RTM_GET,
+    RTM_VERSION, SOCK_RAW,
+};
+
+use crate::default_err;
+
+pub fn interface_and_mtu_impl(remote: IpAddr) -> Result<(String, usize), Error> {
+    // Open route socket.
+    let fd = unsafe { socket(PF_ROUTE, SOCK_RAW, 0) };
+    if fd == -1 {
+        return Err(Error::last_os_error());
+    }
+
+    // Prepare buffer with destination `sockaddr`.
+    #[allow(clippy::cast_possible_truncation)]
+    let (remote, len) = match remote {
+        IpAddr::V4(ip) => {
+            let mut sin: sockaddr_in = unsafe { mem::zeroed() };
+            sin.sin_len = size_of::<sockaddr_in>() as u8;
+            sin.sin_family = AF_INET as u8;
+            sin.sin_addr.s_addr = u32::from_ne_bytes(ip.octets());
+            (
+                ptr::from_ref::<sockaddr_in>(&sin).cast::<u8>(),
+                size_of::<sockaddr_in>(),
+            )
+        }
+        IpAddr::V6(ip) => {
+            let mut sin6: sockaddr_in6 = unsafe { mem::zeroed() };
+            sin6.sin6_len = size_of::<sockaddr_in6>() as u8;
+            sin6.sin6_family = AF_INET6 as u8;
+            sin6.sin6_addr.s6_addr = ip.octets();
+            (
+                ptr::from_ref::<sockaddr_in6>(&sin6).cast::<u8>(),
+                size_of::<sockaddr_in6>(),
+            )
+        }
+    };
+
+    // Prepare route message structure.
+    let mut rtm: rt_msghdr = unsafe { mem::zeroed() };
+    #[allow(clippy::cast_possible_truncation)]
+    {
+        rtm.rtm_msglen = (size_of::<rt_msghdr>() + len) as u16; // Length includes sockaddr
+        rtm.rtm_version = RTM_VERSION as u8;
+        rtm.rtm_type = RTM_GET as u8;
+    }
+    rtm.rtm_seq = fd; // Abuse file descriptor as sequence number, since it's unique
+    rtm.rtm_addrs = RTA_DST | RTA_IFP; // Query for destination and obtain interface info
+
+    // Copy route message and destination `sockaddr` into message buffer.
+    let mut msg: Vec<u8> = vec![0; rtm.rtm_msglen as usize];
+    unsafe {
+        ptr::copy_nonoverlapping(
+            ptr::from_ref::<rt_msghdr>(&rtm).cast::<u8>(),
+            msg.as_mut_ptr(),
+            size_of::<rt_msghdr>(),
+        );
+        ptr::copy_nonoverlapping(remote, msg.as_mut_ptr().add(size_of::<rt_msghdr>()), len);
+    }
+
+    // Send route message.
+    let res = unsafe { write(fd, msg.as_ptr().cast::<c_void>(), msg.len()) };
+    if res == -1 {
+        unsafe { close(fd) };
+        return Err(Error::last_os_error());
+    }
+
+    // Read route messages.
+    let mut buf = vec![
+        0u8;
+        size_of::<rt_msghdr>() +
+        // There will never be `RTAX_MAX` sockaddrs, but it's a safe upper bound
+         (RTAX_MAX as usize * size_of::<sockaddr_storage>())
+    ];
+    let rtm = loop {
+        let len = unsafe { read(fd, buf.as_mut_ptr().cast::<c_void>(), buf.len()) };
+        if len <= 0 {
+            unsafe { close(fd) };
+            return Err(Error::last_os_error());
+        }
+        let rtm = unsafe { ptr::read_unaligned(buf.as_ptr().cast::<rt_msghdr>()) };
+        #[allow(clippy::cast_possible_truncation)]
+        if rtm.rtm_type == RTM_GET as u8 && rtm.rtm_pid == unsafe { getpid() } && rtm.rtm_seq == fd
+        {
+            // This is the response we are looking for.
+            break rtm;
+        }
+    };
+
+    // Close the route socket.
+    unsafe { close(fd) };
+
+    // Parse the route message for the interface name.
+    let mut sa = unsafe { buf.as_ptr().add(size_of::<rt_msghdr>()) };
+    for i in 0..RTAX_MAX {
+        let sdl = unsafe { ptr::read_unaligned(sa.cast::<sockaddr_dl>()) };
+        // Check if the address is present in the message
+        if rtm.rtm_addrs & (1 << i) != 0 {
+            // Check if the address is the interface address
+            if i == RTAX_IFP {
+                if let Ok(name) = unsafe {
+                    str::from_utf8(std::slice::from_raw_parts(
+                        sdl.sdl_data.as_ptr().cast::<u8>(),
+                        sdl.sdl_nlen as usize,
+                    ))
+                } {
+                    // We have our interface name.
+                    return Ok((name.to_string(), rtm.rtm_rmx.rmx_mtu as usize));
+                }
+            }
+            // Advance to the next address. The length is always a multiple of 4, so we need to do
+            // some awkward manipulation.
+            sa = unsafe {
+                sa.add(if sdl.sdl_len == 0 {
+                    4
+                } else {
+                    ((sdl.sdl_len - 1) | 3) + 1
+                } as usize)
+            };
+        }
+    }
+
+    Err(default_err())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,56 +6,39 @@
 
 use std::{
     io::{Error, ErrorKind},
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket},
+    net::IpAddr,
 };
 
-// Though the module includes `allow(clippy::all)`, that doesn't seem to affect some lints
-#[allow(clippy::semicolon_if_nothing_returned, clippy::struct_field_names)]
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+))]
+use bsd::interface_and_mtu_impl;
+
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+))]
+mod bsd;
+
 #[cfg(windows)]
 mod win_bindings;
 
-/// Prepare a default error result.
-fn default_result<T>() -> Result<(String, T), Error> {
-    Err(Error::new(
-        ErrorKind::NotFound,
-        "Local interface MTU not found",
-    ))
-}
-
-#[derive(Debug)]
-pub enum SocketAddrs {
-    Local(IpAddr),
-    Remote(SocketAddr),
-    Both((IpAddr, SocketAddr)),
-}
-
-impl From<&(IpAddr, SocketAddr)> for SocketAddrs {
-    fn from((local, remote): &(IpAddr, SocketAddr)) -> Self {
-        Self::Both((*local, *remote))
-    }
-}
-
-impl From<&(Option<IpAddr>, SocketAddr)> for SocketAddrs {
-    fn from((local, remote): &(Option<IpAddr>, SocketAddr)) -> Self {
-        local.map_or(Self::Remote(*remote), |local| Self::Both((local, *remote)))
-    }
-}
-
-impl From<&(IpAddr, Option<SocketAddr>)> for SocketAddrs {
-    fn from((local, remote): &(IpAddr, Option<SocketAddr>)) -> Self {
-        remote.map_or(Self::Local(*local), |remote| Self::Both((*local, remote)))
-    }
+/// Prepare a default error.
+fn default_err() -> Error {
+    Error::new(ErrorKind::NotFound, "Local interface MTU not found")
 }
 
 /// Return the name and maximum transmission unit (MTU) of a local network interface.
 ///
-/// Given a pair of local and remote [`SocketAddr`]s, return the name and maximum
-/// transmission unit (MTU) of the local network interface used by a socket bound to the local
-/// address and connected towards the remote destination.
-///
-/// If the local address is `None`, the function will let the operating system choose the local
-/// address based on the given remote address. If the remote address is `None`, the function will
-/// return the MTU of the local network interface with the given local address.
+/// Given a remote [`IpAddr`], return the name and maximum transmission unit (MTU) of the local
+/// network interface towards the remote destination.
 ///
 /// The returned MTU may exceed the maximum IP packet size of 65,535 bytes on some
 /// platforms for some remote destinations. (For example, loopback destinations on
@@ -66,269 +49,22 @@ impl From<&(IpAddr, Option<SocketAddr>)> for SocketAddrs {
 /// # Examples
 ///
 /// ```
-/// let saddr = "127.0.0.1:443".parse().unwrap();
-/// let (name, mtu) = mtu::interface_and_mtu(&(None, saddr)).unwrap();
-/// println!("MTU towards {saddr:?} is {mtu} on {name}");
+/// let remote = "127.0.0.1".parse().unwrap();
+/// let (name, mtu) = mtu::interface_and_mtu(remote).unwrap();
+/// println!("MTU towards {remote:?} is {mtu} on {name}");
 /// ```
 ///
 /// # Errors
 ///
 /// This function returns an error if the local interface MTU cannot be determined.
-pub fn interface_and_mtu<A>(addrs: A) -> Result<(String, usize), Error>
-where
-    SocketAddrs: From<A>,
-{
-    let addrs = SocketAddrs::from(addrs);
-    let local = match addrs {
-        SocketAddrs::Local(local) | SocketAddrs::Both((local, _)) => local,
-        SocketAddrs::Remote(remote) => {
-            if remote.is_ipv4() {
-                IpAddr::V4(Ipv4Addr::UNSPECIFIED)
-            } else {
-                IpAddr::V6(Ipv6Addr::UNSPECIFIED)
-            }
-        }
-    };
-    // Let the OS choose an unused local port.
-    let socket = UdpSocket::bind(SocketAddr::new(local, 0))?;
-    match addrs {
-        SocketAddrs::Local(_) => {}
-        SocketAddrs::Remote(remote) | SocketAddrs::Both((_, remote)) => {
-            socket.connect(remote)?;
-        }
-    }
-    interface_and_mtu_impl(&socket)
-}
-
-#[cfg(not(any(
-    target_os = "macos",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "linux",
-    target_os = "windows"
-)))]
-fn interface_and_mtu_impl(_socket: &UdpSocket) -> Result<(String, usize), Error> {
-    default_result()
-}
-
-#[cfg(any(
-    target_os = "macos",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "linux"
-))]
-fn interface_and_mtu_impl(socket: &UdpSocket) -> Result<(String, usize), Error> {
-    #[cfg(target_os = "linux")]
-    use std::{ffi::c_char, mem, os::fd::AsRawFd};
-    use std::{
-        ffi::{c_int, CStr},
-        ptr,
-    };
-
-    use libc::{
-        freeifaddrs, getifaddrs, ifaddrs, in_addr_t, sockaddr_in, sockaddr_in6, AF_INET, AF_INET6,
-    };
-    #[cfg(any(
-        target_os = "macos",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_os = "openbsd"
-    ))]
-    use libc::{if_data, AF_LINK};
-    #[cfg(target_os = "linux")]
-    use libc::{ifreq, ioctl};
-
-    // Get the interface list.
-    let mut ifap: *mut ifaddrs = ptr::null_mut();
-    if unsafe { getifaddrs(&mut ifap) } != 0 {
-        return Err(Error::last_os_error());
-    }
-    let ifap = ifap; // Do not modify this pointer.
-
-    // First, find the name of the interface with the local IP address determined above.
-    let mut cursor = ifap;
-    let iface = loop {
-        if cursor.is_null() {
-            break None;
-        }
-
-        let ifa = unsafe { &*cursor };
-        if !ifa.ifa_addr.is_null() {
-            let saddr = unsafe { &*ifa.ifa_addr };
-            if matches!(c_int::from(saddr.sa_family), AF_INET | AF_INET6)
-                && match socket.local_addr()?.ip() {
-                    IpAddr::V4(ip) => {
-                        let saddr: sockaddr_in =
-                            unsafe { ptr::read_unaligned(ifa.ifa_addr.cast::<sockaddr_in>()) };
-                        saddr.sin_addr.s_addr == in_addr_t::to_be(ip.into())
-                    }
-                    IpAddr::V6(ip) => {
-                        let saddr: sockaddr_in6 =
-                            unsafe { ptr::read_unaligned(ifa.ifa_addr.cast::<sockaddr_in6>()) };
-                        saddr.sin6_addr.s6_addr == ip.octets()
-                    }
-                }
-            {
-                break unsafe { CStr::from_ptr(ifa.ifa_name).to_str().ok() };
-            }
-        }
-        cursor = ifa.ifa_next;
-    };
-
-    // If we have found the interface name we are looking for, find the MTU.
-    let mut res = default_result();
-    if let Some(iface) = iface {
-        #[cfg(any(
-            target_os = "macos",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd"
-        ))]
-        {
-            // On macOS, we need to loop again to find the MTU of that interface. We need to
-            // do two loops, because `getifaddrs` returns one entry per
-            // interface and link type, and the IP addresses are in the
-            // AF_INET/AF_INET6 entries for an interface, whereas the
-            // MTU is (only) in the AF_LINK entry, whose `ifa_addr`
-            // contains MAC address information, not IP address
-            // information.
-            let mut cursor = ifap;
-            while !cursor.is_null() {
-                let ifa = unsafe { &*cursor };
-                if !ifa.ifa_addr.is_null() {
-                    let saddr = unsafe { &*ifa.ifa_addr };
-                    let name =
-                        String::from_utf8_lossy(unsafe { CStr::from_ptr(ifa.ifa_name).to_bytes() });
-                    if c_int::from(saddr.sa_family) == AF_LINK
-                        && !ifa.ifa_data.is_null()
-                        && name == iface
-                    {
-                        let data = unsafe { &*(ifa.ifa_data as *const if_data) };
-                        if let Ok(mtu) = usize::try_from(data.ifi_mtu) {
-                            res = Ok((iface.to_string(), mtu));
-                        }
-                        break;
-                    }
-                }
-                cursor = ifa.ifa_next;
-            }
-        }
-
-        #[cfg(target_os = "linux")]
-        {
-            // On Linux, we can get the MTU via an ioctl on the socket.
-            let mut ifr: ifreq = unsafe { mem::zeroed() };
-            ifr.ifr_name[..iface.len()].copy_from_slice(unsafe {
-                &*(ptr::from_ref::<[u8]>(iface.as_bytes()) as *const [c_char])
-            });
-            if unsafe { ioctl(socket.as_raw_fd(), libc::SIOCGIFMTU, &ifr) } != 0 {
-                res = Err(Error::last_os_error());
-            } else if let Ok(mtu) = usize::try_from(unsafe { ifr.ifr_ifru.ifru_mtu }) {
-                res = Ok((iface.to_string(), mtu));
-            }
-        }
-    }
-
-    unsafe { freeifaddrs(ifap) };
-    res
-}
-
-#[cfg(target_os = "windows")]
-fn interface_and_mtu_impl(socket: &UdpSocket) -> Result<(String, usize), Error> {
-    use std::{
-        ffi::{c_void, CStr},
-        ptr, slice,
-    };
-
-    use win_bindings::{
-        if_indextoname, FreeMibTable, GetIpInterfaceTable, GetUnicastIpAddressTable, AF_INET,
-        AF_INET6, AF_UNSPEC, MIB_IPINTERFACE_ROW, MIB_IPINTERFACE_TABLE, MIB_UNICASTIPADDRESS_ROW,
-        MIB_UNICASTIPADDRESS_TABLE, NO_ERROR,
-    };
-
-    let mut res = default_result();
-
-    // Get a list of all unicast IP addresses with associated metadata.
-    let mut addr_table: *mut MIB_UNICASTIPADDRESS_TABLE = ptr::null_mut();
-    if unsafe { GetUnicastIpAddressTable(AF_UNSPEC, &mut addr_table) } != NO_ERROR {
-        return Err(Error::last_os_error());
-    }
-    let addr_table = addr_table; // Do not modify this pointer.
-
-    let addrs = unsafe {
-        slice::from_raw_parts::<MIB_UNICASTIPADDRESS_ROW>(
-            &(*addr_table).Table[0],
-            (*addr_table).NumEntries as usize,
-        )
-    };
-
-    // Get a list of all interfaces with associated metadata.
-    let mut if_table: *mut MIB_IPINTERFACE_TABLE = ptr::null_mut();
-    if unsafe { GetIpInterfaceTable(AF_UNSPEC, &mut if_table) } != NO_ERROR {
-        let error = Error::last_os_error();
-        unsafe { FreeMibTable(addr_table as *const c_void) };
-        return Err(error);
-    }
-    let if_table = if_table; // Do not modify this pointer.
-
-    let ifaces = unsafe {
-        slice::from_raw_parts::<MIB_IPINTERFACE_ROW>(
-            &(*if_table).Table[0],
-            (*if_table).NumEntries as usize,
-        )
-    };
-
-    // Run through the list of addresses and find the one that matches the local IP
-    // address.
-    'addr_loop: for addr in addrs {
-        let af = unsafe { addr.Address.si_family };
-        let ip = socket.local_addr()?.ip();
-        if (af == AF_INET && ip.is_ipv4() || af == AF_INET6 && ip.is_ipv6())
-            && match ip {
-                IpAddr::V4(ip) => {
-                    u32::from(ip).to_be() == unsafe { addr.Address.Ipv4.sin_addr.S_un.S_addr }
-                }
-                IpAddr::V6(ip) => ip.octets() == unsafe { addr.Address.Ipv6.sin6_addr.u.Byte },
-            }
-        {
-            // For the matching address, find local interface and its MTU.
-            for iface in ifaces {
-                if iface.InterfaceIndex == addr.InterfaceIndex {
-                    if let Ok(mtu) = iface.NlMtu.try_into() {
-                        let mut name = [0u8; 256]; // IF_NAMESIZE not available?
-                        if unsafe { !if_indextoname(iface.InterfaceIndex, &mut name).is_null() } {
-                            if let Ok(name) = CStr::from_bytes_until_nul(&name) {
-                                if let Ok(name) = name.to_str() {
-                                    res = Ok((name.to_string(), mtu));
-                                }
-                            }
-                        } else {
-                            res = Err(Error::last_os_error());
-                        }
-                    }
-                    break 'addr_loop;
-                }
-            }
-        }
-    }
-
-    unsafe { FreeMibTable(if_table as *const c_void) };
-    unsafe { FreeMibTable(addr_table as *const c_void) };
-
-    res
+// #[allow(clippy::missing_panics_doc, clippy::too_many_lines)]
+pub fn interface_and_mtu(remote: IpAddr) -> Result<(String, usize), Error> {
+    interface_and_mtu_impl(remote)
 }
 
 #[cfg(test)]
 mod test {
-    use std::{
-        env,
-        io::ErrorKind,
-        net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket},
-    };
-
-    use rand::Rng;
+    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
     use crate::interface_and_mtu;
 
@@ -355,141 +91,40 @@ mod test {
     // Non-loopback interface names are unpredictable, so we only check the MTU.
     const INET: NameMtu = NameMtu(None, 1_500);
 
-    //  The tests can run in parallel, so try and find unused ports for all the tests.
-    fn socket_with_addr(local_ip: IpAddr) -> SocketAddr {
-        loop {
-            let port = rand::thread_rng().gen_range(1024..65535);
-            let saddr = SocketAddr::new(local_ip, port);
-            let socket = UdpSocket::bind(saddr);
-            match socket {
-                // We found an unused port.
-                Ok(socket) => return socket.local_addr().unwrap(),
-                Err(e) => match e.kind() {
-                    ErrorKind::AddrInUse | ErrorKind::PermissionDenied => {
-                        // We hit a used or priviledged port, try again.
-                        continue;
-                    }
-                    _ => {
-                        // We hit another error. Pretend that worked by returning the socket
-                        // address, so the actual code can hit the same error.
-                        return saddr;
-                    }
-                },
-            }
-        }
+    #[test]
+    fn loopback_v4() {
+        assert_eq!(
+            interface_and_mtu(IpAddr::V4(Ipv4Addr::LOCALHOST)).unwrap(),
+            LOOPBACK
+        );
     }
 
-    fn local_v4() -> SocketAddr {
-        socket_with_addr(IpAddr::V4(Ipv4Addr::LOCALHOST))
+    #[test]
+    fn loopback_v6() {
+        assert_eq!(
+            interface_and_mtu(IpAddr::V6(Ipv6Addr::LOCALHOST)).unwrap(),
+            LOOPBACK
+        );
     }
 
-    fn local_v6() -> SocketAddr {
-        socket_with_addr(IpAddr::V6(Ipv6Addr::LOCALHOST))
-    }
-
-    fn inet_v4() -> SocketAddr {
+    #[test]
+    fn inet_v4() {
         // cloudflare.com
-        socket_with_addr(IpAddr::V4(Ipv4Addr::new(104, 16, 132, 229)))
+        assert_eq!(
+            interface_and_mtu(IpAddr::V4(Ipv4Addr::new(104, 16, 132, 229))).unwrap(),
+            INET
+        );
     }
 
-    fn inet_v6() -> SocketAddr {
+    #[test]
+    fn inet_v6() {
         // cloudflare.com
-        socket_with_addr(IpAddr::V6(Ipv6Addr::new(
-            0x26, 0x06, 0x47, 0x00, 0x68, 0x10, 0x84, 0xe5,
-        )))
-    }
-
-    #[test]
-    fn loopback_v4_loopback_v4() {
         assert_eq!(
-            interface_and_mtu(&(local_v4().ip(), local_v4())).unwrap(),
-            LOOPBACK
+            interface_and_mtu(IpAddr::V6(Ipv6Addr::new(
+                0x26, 0x06, 0x47, 0x00, 0x68, 0x10, 0x84, 0xe5,
+            )))
+            .unwrap(),
+            INET
         );
-    }
-
-    #[test]
-    fn loopback_v4_loopback_v6() {
-        assert!(interface_and_mtu(&(local_v4().ip(), local_v6())).is_err());
-    }
-
-    #[test]
-    fn loopback_v6_loopback_v4() {
-        assert!(interface_and_mtu(&(local_v6().ip(), local_v4())).is_err());
-    }
-
-    #[test]
-    fn loopback_v6_loopback_v6() {
-        assert_eq!(
-            interface_and_mtu(&(local_v6().ip(), local_v6())).unwrap(),
-            LOOPBACK
-        );
-    }
-    #[test]
-    fn none_loopback_v4() {
-        assert_eq!(interface_and_mtu(&(None, local_v4())).unwrap(), LOOPBACK);
-    }
-
-    #[test]
-    fn none_loopback_v6() {
-        assert_eq!(interface_and_mtu(&(None, local_v6())).unwrap(), LOOPBACK);
-    }
-
-    #[test]
-    fn loopback_v4_none() {
-        assert_eq!(
-            interface_and_mtu(&(local_v4().ip(), None)).unwrap(),
-            LOOPBACK
-        );
-    }
-
-    #[test]
-    fn loopback_v6_none() {
-        assert_eq!(
-            interface_and_mtu(&(local_v6().ip(), None)).unwrap(),
-            LOOPBACK
-        );
-    }
-
-    #[test]
-    fn inet_v4_inet_v4() {
-        assert!(interface_and_mtu(&(inet_v4().ip(), inet_v4())).is_err());
-    }
-
-    #[test]
-    fn inet_v4_inet_v6() {
-        assert!(interface_and_mtu(&(inet_v4().ip(), inet_v6())).is_err());
-    }
-
-    #[test]
-    fn inet_v6_inet_v4() {
-        assert!(interface_and_mtu(&(inet_v6().ip(), inet_v4())).is_err());
-    }
-
-    #[test]
-    fn inet_v6_inet_v6() {
-        assert!(interface_and_mtu(&(inet_v6().ip(), inet_v6())).is_err());
-    }
-    #[test]
-    fn none_inet_v4() {
-        assert_eq!(interface_and_mtu(&(None, inet_v4())).unwrap(), INET);
-    }
-
-    #[test]
-    fn none_inet_v6() {
-        if env::var("GITHUB_ACTIONS").is_ok() {
-            // The GitHub CI environment does not have IPv6 connectivity.
-            return;
-        }
-        assert_eq!(interface_and_mtu(&(None, inet_v6())).unwrap(), INET);
-    }
-
-    #[test]
-    fn inet_v4_none() {
-        assert!(interface_and_mtu(&(inet_v4().ip(), None)).is_err());
-    }
-
-    #[test]
-    fn inet_v6_none() {
-        assert!(interface_and_mtu(&(inet_v6().ip(), None)).is_err());
     }
 }
