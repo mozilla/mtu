1d1cb0e5968f5c06db0a2fda2cb6891a6fdcf62a
Lars Eggert <lars@eggert.org>
2025-03-12 14:19:33 +0200

    fix: Simplify Android build
    WIP

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ use std::env;
 const BINDINGS: &str = "bindings.rs";
 
 #[cfg(feature = "gecko")]
-fn clang_args(_target_os: &str) -> Vec<String> {
+fn clang_args() -> Vec<String> {
     use mozbuild::{config::BINDGEN_SYSTEM_FLAGS, TOPOBJDIR};
 
     let mut flags: Vec<String> = BINDGEN_SYSTEM_FLAGS.iter().map(|s| s.to_string()).collect();
@@ -27,18 +27,6 @@ fn clang_args(_target_os: &str) -> Vec<String> {
             .to_string(),
     );
     flags
-}
-
-#[cfg(not(feature = "gecko"))]
-fn clang_args(target_os: &str) -> Vec<String> {
-    match target_os {
-        "android" => {
-            let sysroot =
-                env::var("CARGO_NDK_SYSROOT_PATH").expect("CARGO_NDK_SYSROOT_PATH was not set");
-            vec![format!("--sysroot={sysroot}")]
-        }
-        _ => Vec::new(),
-    }
 }
 
 fn bindgen() {
@@ -72,7 +60,7 @@ fn bindgen() {
     };
 
     let bindings = bindings
-        .clang_args(clang_args(&target_os))
+        .clang_args(clang_args())
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
